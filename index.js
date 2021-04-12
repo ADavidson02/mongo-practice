d93d54f6bb33879478c3647dd9710492f94cb4e2
Amanda Davidson <AmandaADavidson@hotmail.com>
2021-04-12 15:37:05 -0600

    Add function to create multiple listings

--- index.js
+++ index.js
@@ -11,14 +11,29 @@ async function main() {
     //Connect to MongoDB cluster
     await client.connect()
     //Make appropriate DB calls
-    await createListing(client,
-      {
-        name: 'Lovely Loft',
-        summary: 'A lovely Loft in Paris',
-        bedrooms: 1,
-        bathrooms: 1
-      },
-    );
+ await createMultileListings(client, [
+   {
+     name: 'Infinite Views',
+     summary: 'Modern home with infinite views from the pool',
+     bedrooms: 5,
+     bathrooms: 4.5,
+     beds: 5,
+   },
+   {
+     name: 'Private room in London',
+     property_type: 'Apartment',
+     bedrooms: 1,
+     bathrooms: 0,
+   },
+   {
+     name: 'Beautiful Beach House',
+     property_type:
+       'Enjoy relaxed beach living in this house with a private beach',
+     bedrooms: 4,
+     bathrooms: 2,
+     last_review: new Date(),
+   },
+ ]);
   } catch(e) {
     console.error(e)
   }finally {
@@ -35,6 +50,18 @@ async function createListing(client, newListing) {
   const result = await client.db("sample_airbnb").collection("listingsAndReviews").insertOne(newListing)
   console.log(`New listing created with the following id: ${result.insertedId}`)
 }
+
+async function createMultileListings(client, newListings) {
+  const result = await client
+    .db('sample_airbnb')
+    .collection('listingsAndReviews')
+    .insertMany(newListings);
+  console.log(
+    `${result.insertedCount} new listing(s) crated with the following id(s):`
+  );
+  console.log(result.insertedIds);
+}
+
 async function listDatabases(client){
   const databasesList = await client.db().admin().listDatabases()
 
